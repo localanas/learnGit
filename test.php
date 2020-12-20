1aab7d49d6f6cbedba4aeed3bde1f6a1c5371506
anass-hiit <anass.n@hiitconsulting.com>
2020-12-20 20:16:42 +0100

    test rebasing
    dollar 3
    
    my commit

--- test.php
+++ test.php
@@ -9,3 +9,16 @@ echo "my modif with my branch ";
 echo "my test depuis remote  ";
 echo "auhtre test";
 echo  "elonier a develop";
+echo "dollar";
+echo "dollar 2";
+echo "dollard 3 in master";
+<<<<<<< HEAD
+echo "dollard 4 in au author conflict";
+=======
+echo "dollar 3";
+<<<<<<< HEAD
+
+>>>>>>> dollar 3
+=======
+echo "rebasing dollar 3 in local";
+>>>>>>> my commit
